6936f53a8676a11f1b8d5419a700900e3d6f38bc
gohittanya25 <gohittanya11@gmail.com>
2021-07-03 13:57:44 +0530

    Initial Commit

---
+++ src/test/java/com/karkinos/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karkinos.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
